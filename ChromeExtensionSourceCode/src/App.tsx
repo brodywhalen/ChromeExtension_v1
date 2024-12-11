79e206c9848d8647d5635968cbabed928deb9037
brodywhalen <123722295+brodywhalen@users.noreply.github.com>
2024-12-11 02:38:00 -0600

    added start of keypress handler... need to find a way to click on to iframe/canvas dom

--- ChromeExtensionSourceCode/src/App.tsx
+++ ChromeExtensionSourceCode/src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import { IframeHTMLAttributes } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  const onclick = async() => {
+    const [tab] = await chrome.tabs.query({active:true, lastFocusedWindow: true})
+    console.log(tab);
+
+    setTimeout(function(){console.log('waiting over')}, 1500);
+ 
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id!},
+      func: () => {
+
+      const keyEvent = new KeyboardEvent('keypress', {bubbles: true, cancelable: true, key: 'b'})
+      // keyEvent.key = KEY // A key like 'a' or 'B' or 'Backspace'
+      // You will need to change this line if you want to use other special characters such as the left and right arrows
+      // keyEvent.keyCode = KEY.charCodeAt(0) 
+      console.log('keyEvent: ', keyEvent);
+      
+      setTimeout(function(){document.querySelector<HTMLIFrameElement>('.docs-texteventtarget-iframe')!.contentDocument!.activeElement!.dispatchEvent(keyEvent)
+        console.log('waiting completed')
+      }, 1000) // exlamation points for anti null assertion correct?
+      
+
+        // alert('hello from my extension')
+      }
+    })
+  }
 
   return (
     <>
@@ -18,8 +45,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={onclick}>
+          Click Me
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
